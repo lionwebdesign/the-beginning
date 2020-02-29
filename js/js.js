9176c2051e4623fabed8e496a77bd69443fbddf9
Ruben <vim.ruben@hotmail.com>
2020-02-28 22:55:07 -0300

    Primera edicion del template dinamico como muestra futura

---
+++ js/js.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slider();
+    // var height = $(window).height();
+  
+    // $('#div2').height(height);
+  });
+  
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
